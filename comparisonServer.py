f0d90746734d8cc248257ea3b970085549f53c6e
Fábio M. Costa <fmc@ufg.br>
2026-03-02 10:26:22 -0300

    Refactor client socket creation in mainLoop
    Refactored client socket handling to create a new socket for each connection.

--- comparisonServer.py
+++ comparisonServer.py
@@ -10,9 +10,9 @@ serverSock.listen(6)
 
 def mainLoop():
 	cont = 1
-	clientSock = socket(AF_INET, SOCK_STREAM)
 	while 1:
 		nMsgs = promptUser()
+		clientSock = socket(AF_INET, SOCK_STREAM)
 		clientSock.connect((GROUPMNGR_ADDR,GROUPMNGR_TCP_PORT))
 		req = {"op":"list"}
 		msg = pickle.dumps(req)
@@ -30,11 +30,12 @@ def mainLoop():
 			serverSock.close()
 			
 			# Tell group manager to stop
-			clientSock.connect((GROUPMNGR_ADDR,GROUPMNGR_TCP_PORT))
+			cSock = socket(AF_INET, SOCK_STREAM)
+			cSock.connect((GROUPMNGR_ADDR,GROUPMNGR_TCP_PORT))
 			req = {"op":"stop"}
 			msg = pickle.dumps(req)
-			clientSock.send(msg)
-			clientSock.close()
+			cSock.send(msg)
+			cSock.close()
 			break
 
 def promptUser():
